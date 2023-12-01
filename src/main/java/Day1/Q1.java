b5d85ae79752e49b7a2f70a3cca822b9afe76d06
Ciaran <irishrocker182@gmail.com>
2023-12-01 09:52:31 +0000

    AOC part 1 and 2 - First pass solution

---
+++ src/main/java/Day1/Q1.java
@@ -0,0 +1,67 @@
+package Day1;
+
+import util.FileReader;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Q1 {
+
+    private static final HashMap<String, Integer> stringToIntValues = new HashMap<>();
+
+    private static final String validNumberStartingChars = "otfsen";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        createNumberMap();
+        FileReader fileReader = new FileReader("1");
+        ArrayList<String> inputLines = fileReader.readFileIntoListOfStrings();
+
+        int sum = 0;
+        for(String line : inputLines){
+            ArrayList<String> numbers = new ArrayList<>();
+            for(int i = 0 ; i < line.length();i++){
+                char currentCharacter =  line.charAt(i);
+
+                if(Character.isDigit(currentCharacter)){
+                    numbers.add(String.valueOf(currentCharacter));
+                }
+                if(isValidNumberString(currentCharacter)){
+                    String searchForwardOnly = line.substring(i);
+
+                    for (String key : stringToIntValues.keySet()) {
+                        if(searchForwardOnly.indexOf(key) == 0){
+                            numbers.add(String.valueOf(stringToIntValues.get(key)));
+                            break;
+                        }
+                    }
+                }
+            }
+            int combinedValue = Integer.valueOf((numbers.getFirst() + numbers.getLast()));
+            sum += combinedValue;
+            System.out.println(combinedValue);
+        }
+
+        System.out.println(sum);
+
+    }
+
+    private static boolean isValidNumberString(char inputChar){
+        return validNumberStartingChars.contains(String.valueOf(inputChar));
+
+    }
+
+    private static void createNumberMap(){
+        stringToIntValues.put("one",1);
+        stringToIntValues.put("two",2);
+        stringToIntValues.put("three",3);
+        stringToIntValues.put("four",4);
+        stringToIntValues.put("five",5);
+        stringToIntValues.put("six",6);
+        stringToIntValues.put("seven",7);
+        stringToIntValues.put("eight",8);
+        stringToIntValues.put("nine",9);
+    }
+}
+
+
--- src/main/java/util/FileReader.java
+++ src/main/java/util/FileReader.java
@@ -1,4 +1,46 @@
 package util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FileReader {
+
+    private String path = "src/main/java/Day%/input.txt";
+
+    public FileReader(String inputPath,boolean usePaths) throws IOException {
+        Path filePath = Paths.get(inputPath);
+
+        //Stream
+        Files.lines(filePath).forEach(System.out::println);
+    }
+    public FileReader(String day){
+        path = path.replace("%",day);
+    }
+
+    public ArrayList<Integer> readFileIntoListOfInts() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(path));
+        ArrayList<Integer> lines = new ArrayList<>();
+        while(scanner.hasNextLine()) {
+            lines.add(Integer.parseInt(scanner.nextLine()));
+        }
+        scanner.close();
+        return lines;
+    }
+
+    public ArrayList<String> readFileIntoListOfStrings() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(path));
+        ArrayList<String> lines = new ArrayList<>();
+        while(scanner.hasNextLine()) {
+            lines.add(scanner.nextLine());
+        }
+        scanner.close();
+        return lines;
+    }
 }
+
